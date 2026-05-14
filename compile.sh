0c5942b4900a365856df11b8164ad88c8afbb286
felipecostanza <fcostanza@itba.edu.ar>
2026-05-14 21:24:41 +0000

    hol

--- compile.sh
+++ compile.sh
@@ -1,5 +1,9 @@
 docker start DockerPrueba2
+echo 'hola'
 docker exec -it DockerPrueba2 make clean -C /root/Toolchain
+echo 'hol2a'
+
+
 docker exec -it DockerPrueba2 make clean -C /root/
 docker exec -it DockerPrueba2 make -C /root/Toolchain
 docker exec -it DockerPrueba2 make -C /root/
